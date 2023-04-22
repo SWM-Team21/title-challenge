63a5ccc24fec0c73776620f6dee38f0389e2631a
NAM GIBEOM <rlqja0523@naver.com>
2023-04-23 03:00:53 +0900

    init

---
+++ src/main/java/team21/server/TitleChallengeApplication.java
@@ -0,0 +1,13 @@
+package team21.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitleChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitleChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team21/server/TitleChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package team21.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitleChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
