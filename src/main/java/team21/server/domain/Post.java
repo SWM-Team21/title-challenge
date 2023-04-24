3ddcafdc3a1ef13268f0b80c5503e23d005d3ec2
NAM GIBEOM <rlqja0523@naver.com>
2023-04-25 01:39:19 +0900

    fix: @ManyToOne Lazy, @OneToMany 영속성 전이, 고아 객체 옵션 추가

--- src/main/java/team21/server/domain/Post.java
+++ src/main/java/team21/server/domain/Post.java
@@ -3,8 +3,10 @@ package team21.server.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -24,10 +26,10 @@ public class Post {
     @Column(nullable = false)
     private String imageName;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false, name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "post")
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 }
