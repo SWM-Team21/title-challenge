11710388e9498857a21473fa0a1dda46df657760
NAM GIBEOM <rlqja0523@naver.com>
2023-04-25 01:16:11 +0900

    feat: spring security configuration

---
+++ src/main/java/team21/server/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package team21.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import team21.server.auth.enums.Role;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+    private final String ROLE_USER = Role.ROLE_USER.name();
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .csrf().disable()
+                .cors(Customizer.withDefaults())
+                .formLogin()
+                .loginProcessingUrl("/users/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/users/logout"))
+                .and()
+                .authorizeRequests()
+                .antMatchers("/post/**").hasRole(ROLE_USER)
+                .antMatchers("users/mypage/**").hasRole(ROLE_USER)
+                .anyRequest().permitAll();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
