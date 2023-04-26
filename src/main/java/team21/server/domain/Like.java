9f53a6cd5f3af312ecc9d4c7e2e1b1500c467a85
yooniversal <iteratively@naver.com>
2023-04-27 04:02:18 +0900

    fix: 댓글/좋아요 기본 생성자 추가

--- src/main/java/team21/server/domain/Like.java
+++ src/main/java/team21/server/domain/Like.java
@@ -1,12 +1,14 @@
 package team21.server.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
 public class Like {
 
     @Id @GeneratedValue
