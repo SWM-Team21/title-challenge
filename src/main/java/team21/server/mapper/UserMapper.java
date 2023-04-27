4903befc91950c31d47120f1401f7bc4c7293c8b
NAM GIBEOM <rlqja0523@naver.com>
2023-04-27 15:30:18 +0900

    fix: signup mapping 에러 수정

--- src/main/java/team21/server/mapper/UserMapper.java
+++ src/main/java/team21/server/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package team21.server.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import team21.server.domain.User;
 import team21.server.dto.UserDto;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "userId", ignore = true)
     User signupToEntity(UserDto.Signup signupDto);
 }
