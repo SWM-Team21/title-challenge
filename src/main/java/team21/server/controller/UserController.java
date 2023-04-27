821340df7d002deb553455431624b85f13882f26
NAM GIBEOM <rlqja0523@naver.com>
2023-04-27 15:57:05 +0900

    feat: 사용자 이미지 업로드

--- src/main/java/team21/server/controller/UserController.java
+++ src/main/java/team21/server/controller/UserController.java
@@ -3,15 +3,19 @@ package team21.server.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import team21.server.auth.details.UserDetailsImpl;
 import team21.server.dto.UserDto;
 import team21.server.mapper.UserMapper;
 import team21.server.service.UserService;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/users")
@@ -24,5 +28,13 @@ public class UserController {
     public ResponseEntity signup(@Valid @RequestBody UserDto.Signup signupDto) {
         userService.createUser(mapper.signupToEntity(signupDto));
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/image")
+    public ResponseEntity updateUserImage(@Valid @RequestBody MultipartFile file,
+                                          @AuthenticationPrincipal UserDetailsImpl principal) throws IOException {
+        long userId = principal.getUserId();
+        userService.updateImage(userId, file);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/team21/server/service/UserService.java
+++ src/main/java/team21/server/service/UserService.java
@@ -3,11 +3,14 @@ package team21.server.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import team21.server.aop.BusinessLogicException;
 import team21.server.auth.utils.UserAuthorityUtil;
 import team21.server.domain.User;
 import team21.server.repository.UserRepository;
+import team21.server.utils.FileUtil;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Service
@@ -16,6 +19,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserAuthorityUtil userAuthorityUtil;
+    private final FileUtil fileUtil;
 
     public void createUser(User user) {
         verifyDuplicatedLoginIdExists(user.getLoginId());
@@ -38,5 +42,18 @@ public class UserService {
         if (userRepository.existsByNickName(nickName)) {
             throw new BusinessLogicException("중복된 닉네임입니다.");
         }
+    }
+
+    public void updateImage(long userId, MultipartFile file) throws IOException {
+        User user = findUserById(userId);
+        String imageName = fileUtil.uploadUserImage(file);
+        user.setImageName(imageName);
+
+        userRepository.save(user);
+    }
+
+    public User findUserById(long userId) {
+        Optional<User> optionalUser = userRepository.findByUserId(userId);
+        return optionalUser.orElseThrow(() -> new BusinessLogicException("존재하지 않은 아이디입니다."));
     }
 }
